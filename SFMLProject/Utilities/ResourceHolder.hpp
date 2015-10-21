a76a1b71fecf989fdac0e24d72d0c3d56c401346
Miroslav Hrnčíř <hrncirmirek@windowslive.com>
2015-10-21 21:39:31 +0200

    Added ResourceHolders Added missing texture

---
+++ SFMLProject/Utilities/ResourceHolder.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <stdexcept>
+
+
+template <typename Resource, typename Identifier>
+class ResourceHolder
+{
+public:
+	void						load(Identifier id, const std::string& filename);
+
+	template <typename Parameter>
+	void						load(Identifier id, const std::string& filename, const Parameter& secondParam);
+
+	Resource&					get(Identifier id);
+	const Resource&				get(Identifier id) const;
+
+
+private:
+	void						insertResource(Identifier id, std::unique_ptr<Resource> resource);
+
+private:
+	std::map<Identifier, std::unique_ptr<Resource>>	m_resourceMap;
+};
